632eae8d07f4870a0e2cf732a18b3fcb506247e5
Sajid Saleem <msajids84@hotmail.com>
2026-05-19 13:51:42 +0500

    Fix the INSTALLED_APPS - Accounts before School

--- core/settings.py
+++ core/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'school',
     'accounts',
+    'school',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
